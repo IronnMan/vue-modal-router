01a3f96d5f8a9397c93e36cc0c6c00493ef4d4b1
wenlin42 <wenlin42@icloud.com>
2022-10-25 16:16:31 +0000

    add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { routes } from "./router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
